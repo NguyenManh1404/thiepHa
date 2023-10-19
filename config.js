82bf5a8145d630c1cbb216c24c73e51638a49e73
manh.nguyen <manh.nguyen@ManhNguyen-MO.local>
2023-10-19 21:06:44 +0700

    be me

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
-const nameGirl = 'Bé My dễ thương';
+const nameGirl = 'Thuỷ dễ thương !!';
 const giftUrl = "https://www.facebook.com/profile.php?id=100054724557203";
 const eventName = 'Bất ngờ 20/10 nè';
-const titleCard = 'Hello em';
-const contentCard = 'Chúc Linh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const titleCard = 'Hello bạn';
+const contentCard = 'Chúc Thuỷ 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với bạn trong hôm nay và cả những ngày sau';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = "bemy.png";
+const giftImage = "thuy.png";
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
